f4e826851db0d5f884049485788eb36bb1e3ac9f
Rockwell Mitchell <rockwellmitchell@hotmail.com>
2024-12-27 17:02:45 -0500

    initial commit

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -35,6 +35,10 @@ const CartItem = ({ onContinueShopping }) => {
     return Number(item.cost.substring(1))*item.quantity;
   };
 
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
+
   return (
     <div className="cart-container">
       <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
@@ -60,7 +64,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={handleCheckoutShopping}>Checkout</button>
       </div>
     </div>
   );
